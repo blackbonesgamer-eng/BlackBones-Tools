5dc2c542004fc179b1addea03a00b58a2649ecfe
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-20 17:03:15 -0500

    Add files via upload

---
+++ tokens/Marvel Spiderman-1817070.lua
@@ -0,0 +1,24 @@
+-- Marvel’s Spide​​‌‌‌​​​​​‌‌​‌‌​​​‌‌​‌‌‌​​‌‌​‌​‌​​‌‌​‌‌‌​​‌‌​​​​​​‌‌​‌‌‌​‌‌​​​​‌​​‌‌​‌​‌​‌‌​​​‌‌​‌‌​​‌‌​​​‌‌​​‌​​​‌‌​‌​‌​​‌‌​​‌‌​​‌‌​‌‌​​​‌‌​​​​r-Man Remastered
+-- AppID 1817070 | Generated on 2026-02-20 22:00 UTC | openlua.cloud
+
+-- Main Application
+addappid(1817070, 1, "5d889f9e52a4957f1c3853310182be31551ec3aef31dffa31c3155d4535937ef")
+
+-- Content Depots (14)
+addappid(1817071, 1, "490a357029d1621deff9708e5d42f19324ccc758124ae1b455e091551df2a041") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1817072, 1, "ae2589c0e9de7a18dea92ceac08a6537cf7b83c1983e9f4738a188a49fd137f9") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1817074, 1, "d1d379b15dba708a435fa177bacfc7f625c53c84b45b299ded3cbeeca084e330") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1817075, 1, "ecb149a90bf5eda310dd5f18e7715c255b022db3e327037cc6daa4f69ff22107") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1817076, 1, "6cb4d05622fdd2cd0d10703a36cf47140a2d952a49e31325a3b65af53db59974") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1817077, 1, "3e7903be905c0a12045d1f7d303e596ca9889e64b0b2f4a5e52680e7839b20fb") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1817078, 1, "9815d29285202efd418b703002a67cf17ccde07230fe3f2b171e1581d17ba0e4") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1817079, 1, "e357c3433e694ca3c616af966efc889f3d6af51c4d45e34fbf3d6537715c9bf0") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1829890, 1, "fc8ce2606acf5525e88dffc6cdc9cd4cf50e092efa04536528ee66fda012fe79") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1829891, 1, "69db276eece234ce4caf8b55262d17c74686450104a1d1db09cfdc425555b707") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1829892, 1, "cd4dd166b577600a9fe435ad640a58caa41fb8311111ab403c24a7b1d5a29ed9") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1829893, 1, "48c0acb19e2fc2d02db4b9dcf88fabf25c8eb7eb1e218b7f26c54a53073bb934") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1829894, 1, "1406f2b316ca60231c614560afc407a0b3eca5c0cce4e46f5493e73f269808ef") -- Marvel’s Spider-Man Remastered - Windows
+addappid(1829895, 1, "4773012855296205ad41eeb15231c6d5db4ee4264feb5b217e8b09b752c4526c") -- Marvel’s Spider-Man Remastered - Windows
+
+-- DLCs without Dedicated Depots (1)
+addappid(2083110) -- Marvels Spider-Man Remastered - Pre-purchase Entitlements
