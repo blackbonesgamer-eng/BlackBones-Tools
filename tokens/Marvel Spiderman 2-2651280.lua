2eabf00c47a2c5ae3c612c4603825fad9fb5f376
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-20 17:59:38 -0500

    Add files via upload

---
+++ tokens/Marvel Spiderman 2-2651280.lua
@@ -0,0 +1,26 @@
+-- Marvel's S​​‌‌​‌​‌​​‌‌​‌‌‌​​‌‌​​​‌​​‌‌​‌​‌​​‌‌​​‌‌​‌‌​​​‌‌​​‌‌​​‌‌​​‌‌‌​​‌​​‌‌​‌‌‌​​‌‌​‌‌​​​‌‌​​‌​​‌‌​​​​‌​​‌‌​‌​‌​​‌‌​‌​‌​​‌‌​‌​‌​‌‌​​​‌​pider-Man 2
+-- AppID 2651280 | Generated on 2026-02-20 22:02 UTC | openlua.cloud
+
+-- Main Application
+addappid(2651280, 1, "d191b462673cdb64c982bd00df30c97f296f643cc8a9bb6e5872554700055f4b")
+
+-- Content Depots (15)
+addappid(2651281, 1, "9874571b52ba0c01cbdfd32c5d81920d1093f42fbcf76d02a1e4589746a3b497") -- Marvel's Spider-Man 2 - Windows
+addappid(2651282, 1, "ab459f096f7d0fd0720dd711f19036f532f2678314af4155ed197882c260b539") -- Marvel's Spider-Man 2 - Windows
+addappid(2651283, 1, "e342794b2586f10e0a3273560bda0fb524b448519ff20b6ec938d03e5b949104") -- Marvel's Spider-Man 2 - Windows
+addappid(2651284, 1, "11af403d500753e060bd806ed360e9bf5031e0e846a9ba50a876f68fcb61c641") -- Marvel's Spider-Man 2 - Windows
+addappid(2651285, 1, "14ad0c77584264070ad5cbedf200f8747660475645d0527c93425de942df6d1b") -- Marvel's Spider-Man 2 - Windows
+addappid(2651286, 1, "f33f54c66e41670774919e410838c06145826bfabe00def0753399b0ffe58b4a") -- Marvel's Spider-Man 2 - Windows
+addappid(2651287, 1, "4ab098c2d61d6bc168fb4067cfc2377f9865275310e1e86c960a12de83a39ee9") -- Marvel's Spider-Man 2 - Windows
+addappid(2651288, 1, "0a0e8a531cc2d2483435dcc5d3d0969712b646c0718c39991b0e64d2e0b9a1fd") -- Marvel's Spider-Man 2 - Windows
+addappid(2651289, 1, "14052de118633012aec3fe99b7a692bee3da4c8604d126f7b057f416d327c488") -- Marvel's Spider-Man 2 - Windows
+addappid(2666311, 1, "3bf280f2400be0851c8f7f1872d8c48884df409d8d8e933615ac85b9a972836a") -- Marvel's Spider-Man 2 - Windows
+addappid(2666312, 1, "ecb0cd78998a33161c79c8cc7130a578af813aa46c6d8c54fe7227167ebba919") -- Marvel's Spider-Man 2 - Windows
+addappid(2666313, 1, "4cbc4b49aefb9e6a43312b89474b674d8147e3294baa418b46b3b6c34a9d6850") -- Marvel's Spider-Man 2 - Windows
+addappid(2666314, 1, "58b3013867940ee2998334b9c0412ab2ccc4310a529c231bb74cec034dd2c645") -- Marvel's Spider-Man 2 - Windows
+addappid(2666315, 1, "319c6ed61f3fd2d4571eed5b5132ba16c780d70712db62109e2aa814a912e803") -- Marvel's Spider-Man 2 - Windows
+addappid(2666316, 1, "49480a4027a30684386dc0f2b32494b7424bbbe67234983c07644a0211ccb61c") -- Marvel's Spider-Man 2 - Windows
+
+-- DLCs without Dedicated Depots (2)
+addappid(3172650) -- Marvels Spider-Man 2 - PC Purchase Perks
+addappid(3172660) -- Marvels Spider-Man 2 - Digital Deluxe Upgrade
