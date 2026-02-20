2b5763277c7ceb36de6682110e3daa3297fd50ab
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-20 18:02:07 -0500

    Add files via upload

---
+++ tokens/Wallpaper Engine-431960.lua
@@ -0,0 +1,13 @@
+-- Wallpap​​‌‌​‌​​​​‌‌‌​​​​‌‌​​​‌‌​​‌‌​​​​​‌‌​​‌‌​​‌‌​​‌​‌​​‌‌​‌‌​​​‌‌​​​​​​‌‌​‌‌‌​‌‌​​​‌​​​‌‌​‌‌​​‌‌​​‌​​​​‌‌​​​​​​‌‌​​​​​​‌‌​​‌​​​‌‌​​‌​er Engine
+-- AppID 431960 | Generated on 2026-02-20 21:33 UTC | openlua.cloud
+-- Type: Application
+
+-- Main Application
+addappid(431960, 1, "db983bc58a8cb46d89d1d70cc962cc733956e6124c4025a0c932f82b3c37144a")
+
+-- Content Depots (2)
+addappid(431961, 1, "62ac71786b9fbeecc473fccb8a14da04ea676371addd8820638ee6cddac8c344") -- Wallpaper Engine Content
+addappid(431966, 1, "3217352ccc45b8943d6af58c14d359a38f2897bcf4d2f7f7eb117f79f1887fc4") -- Wallpaper Engine Localization SC
+
+-- DLCs with Content (1)
+addappid(1790230, 1, "28aacfb757c396f07f861f189a2c2ec78653123de2177c05f94a792d1a3c8bfd") -- Wallpaper Engine - Editor Extensions
