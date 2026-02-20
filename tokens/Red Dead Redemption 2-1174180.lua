f397a6f7f8d5247019eb162582a9c7ba8ebaa2af
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-20 17:00:01 -0500

    Add files via upload

---
+++ tokens/Red Dead Redemption 2-1174180.lua
@@ -0,0 +1,20 @@
+-- Red Dead R​​‌‌​​‌​​​‌‌​​​​​​‌‌​‌‌​​​‌‌‌​​‌​​‌‌‌​​​​‌‌​​​​‌​​‌‌​​‌‌​​‌‌‌​​‌​‌‌​​‌​‌​​‌‌​​‌​​‌‌​​‌‌​​‌‌​​‌​​​​‌‌‌​​‌​​‌‌​‌​​​​‌‌​​‌​​​‌‌​‌‌‌edemption 2
+-- AppID 1174180 | Generated on 2026-02-20 17:58 UTC | openlua.cloud
+
+-- Main Application
+addappid(1174180, 1, "c4ba69ff66208b16fc27679ceefb89d490776fc18bae801311a31b6855b74783")
+
+-- Content Depots (6)
+addappid(1174181, 1, "6fbf0f4b33fd5111d0832f0fb63a4d185c5321901ef95776c73e6e10b203bc25") -- Red Dead Redemption 2 Install
+addappid(1174182, 1, "0824e4e00b9d464a1352b0eaa60416ee3bfe0f3b416459991dc6fca87e84366f") -- Red Dead Redemption 2 Base
+addappid(1174183, 1, "cd51ee7e8adde797736bc283f9d434fae0aac28753e3f38a29a78aac298c0ccf") -- Red Dead Redemption 2 Redist
+addappid(1174184, 1, "506c8f971b77f5c39f8baa54fac090e79bf32e19d92eca2a34cba9569bbe5d8a") -- Red Dead Redemption 2 Update
+addappid(1174185, 1, "6fe9c5b4e41f1f91b6a775dd562515e9652570cc607caedef2bc4a9d522fea4d") -- Red Dead Redemption 2 Packs
+addappid(1174186, 1, "0ef7331ad866994474a72131eeb1a5667dc8c9f7be87b4e1d805f288e1d49432") -- Red Dead Redemption 2 Audio
+
+-- Shared Depots (1)
+addappid(1899671, 1, "b7921da5e50d00b2238d0fe870a354cb572bc5d397955fef02a439103f62827b") -- RGL/SC Content (from 1899670)
+
+-- DLCs without Dedicated Depots (2)
+addappid(1190050) -- Red Dead Redemption 2 Special Edition Content
+addappid(1190051) -- Red Dead Redemption 2 Ultimate Edition Content
