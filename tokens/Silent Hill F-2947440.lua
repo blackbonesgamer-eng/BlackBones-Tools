b7d3a4b56b8733f978336d13e1aff22a9df61812
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-20 23:18:31 -0500

    Add files via upload

---
+++ tokens/Silent Hill F-2947440.lua
@@ -0,0 +1,19 @@
+-- SILENT​‌‌​​‌‌​​‌‌​​​‌​​​‌‌​‌‌​​‌‌​​‌​​​​‌‌‌​​​​​‌‌​​​​​​‌‌‌​​‌​​‌‌​​​​​​‌‌‌​​​​‌‌​​‌‌​​‌‌​​‌‌​​​‌‌​‌‌​​‌‌​​​‌​​​‌‌‌​​‌​​‌‌​​​​​​‌‌​​‌‌ HILL f
+-- AppID 2947440 | Generated on 2026-02-21 04:16 UTC | openlua.cloud
+
+-- Main Application
+addappid(2947440, 1, "4f97e37360a346acfc127b9a4cd35dffd337d2f3bde443730f0281eb4943a969")
+
+-- Content Depots (1)
+addappid(2947441, 1, "5329bd39507637997739c33a4b5737f9539ee434a7b79bd875b5fc7d1337c684") -- SILENT HILL f - Content
+
+-- DLCs with Content (1)
+addappid(3282721, 1, "fd38a70c015f046a880fc4db0487fcccdf05e009b286cc4175abdddf61511342") -- SILENT HILL f - Bonus Content
+addtoken(3282720, "186020997252537705")
+
+-- DLCs without Dedicated Depots (5)
+addappid(3282180) -- SILENT HILL f - Pink Rabbit Costume
+addappid(3516200) -- SILENT HILL f - Digital Deluxe Upgrade
+addappid(3282190) -- SILENT HILL f - Omamori Peony
+addappid(3282160) -- SILENT HILL f - White Sailor School Uniform
+addappid(3282170) -- SILENT HILL f - Item Pack
