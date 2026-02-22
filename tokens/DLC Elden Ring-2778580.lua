79985ad9778d7db52dae6bee5e34e77e6a983362
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-21 23:14:07 -0500

    Add files via upload

---
+++ tokens/DLC Elden Ring-2778580.lua
@@ -0,0 +1,9 @@
+-- ELDEN RING Shad​‌‌​​‌​‌​​‌‌​‌​‌​​‌‌‌​​​​‌‌​​‌‌​​​‌‌​‌‌​​​‌‌​​‌​​‌‌​​‌‌​​​‌‌‌​​​​‌‌​​​‌​​‌‌​​​‌​​​‌‌​​​‌​​‌‌​‌​‌​‌‌​​​‌‌​​‌‌​​‌​​‌‌​​​‌‌​​‌‌​‌‌‌ow of the Erdtree
+-- AppID 2778580 | Generated on 2026-02-22 04:06 UTC | openlua.cloud
+-- Type: DLC
+
+-- Base Game: ELDEN RING
+addappid(1245620)
+
+-- Main Application
+addappid(2778580, 1, "9f1556645ea8ef43529f920cf02a2682a6da5756b29e630ba376a0cde24e3908")
