5d868181e2415cb6af397527bd0f3718c513fb36
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-25 19:55:53 -0500

    Add files via upload

---
+++ tokens/God of War Ragnarok-2322010.lua
@@ -0,0 +1,16 @@
+-- God of Wa​​‌‌​​​‌​​‌‌​​‌‌​​‌‌​​‌‌​​‌‌​​​‌​‌‌​​​‌‌​‌‌​​​‌‌​‌‌​​​‌​​​‌‌​‌​‌​​‌‌​​​‌​​‌‌​​​​​​‌‌​​​​​​‌‌‌​​‌​‌‌​​​‌‌​‌‌​​‌​‌​​‌‌​‌‌​​‌‌​​‌​‌r Ragnarök
+-- AppID 2322010 | Generated on 2026-02-26 00:54 UTC | openlua.cloud
+
+-- Main Application
+addappid(2322010, 1, "13d2e9ad8a1b887be0b84504b3ca15dd92617a090c9751c180f8aa04986f3871")
+
+-- Content Depots (1)
+addappid(2322011, 1, "78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49") -- God of War Ragnarök - Content
+
+-- DLCs with Content (1)
+addappid(2974340, 1, "b54ec4dbd1528067329c6ce102d3dc0501401e41b08a6cabc0c947d0c9541baf") -- God of War Ragnark - Digital Artbook
+addtoken(2974340, "14621696841799701455")
+
+-- DLCs without Dedicated Depots (2)
+addappid(2974320) -- God of War Ragnark - Pre-purchase Entitlement
+addappid(2974330) -- God of War Ragnark - Digital Deluxe Edition Upgrade
---
+++ tokens/God of War-1593500.lua
@@ -0,0 +1,8 @@
+-- God ​‌‌​​​‌​​​‌‌​​​​​​‌‌​‌‌‌​‌‌​​‌‌​​‌‌​​​‌​​​‌‌​‌​‌​​‌‌​​‌​​‌‌​​‌‌​​‌‌​​​‌​​​‌‌​​‌‌​​‌‌​​​​​​‌‌​​‌‌​‌‌​​‌‌​​​‌‌‌​​‌​‌‌​​‌​​​‌‌​​​‌​of War
+-- AppID 1593500 | Generated on 2026-02-26 00:52 UTC | openlua.cloud
+
+-- Main Application
+addappid(1593500, 1, "93b91503fb7aaf54f61ae3197cf1790f9889d142399e30f9bd439e7c6a7dc9f8")
+
+-- Content Depots (1)
+addappid(1593501, 1, "77f7390a115063bccc41240f71e0bf27d42c6a7c0424657756fe3f99f26a287a") -- God of War Content
