9a64521daad2cac4e2564764a6c4225324c785c2
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-26 02:40:53 -0500

    Delete complementos/reframework/autorun directory

--- complementos/reframework/autorun/conf/fastersf6_conf.lua
+++
@@ -1,9 +0,0 @@
-local this = {}
-this.TICKER_TIME = 0.75 -- (sec)
-this.SWITCHER_KEY_CODE = 0x20 -- Windows virtual key code
-this.SHOW_FIRST_TICKER = true -- MAKE THIS false => SILENT MODE
-this.FIRST_DESTINATION = 1 -- 1 -> main menu | 2 -> training 
-this.SAFE_MODE = false -- If true, do not agressively skip login part
-this.SAVE_PER_USER = true -- If true, save file is associated to Steam user id(just locally)
-
-return this
