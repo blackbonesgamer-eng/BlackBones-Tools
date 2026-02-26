3c8440e0d8c2f13fdbea847f266efe15f141d38b
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-26 04:08:52 -0500

    Add files via upload

---
+++ tokens/Expedition 33 -1903340.lua
@@ -0,0 +1,11 @@
+-- Clair Obscur:​‌‌​​‌​‌​​‌‌​‌‌‌​‌‌​​​​‌​‌‌​​​​‌​​‌‌​​​​​​‌‌​​‌‌​‌‌​​​‌​​​‌‌‌​​‌​​‌‌​​‌‌​​‌‌​‌​​​​‌‌​​‌‌​​‌‌​‌‌‌​​‌‌​​​​​​‌‌​‌‌‌​‌‌​​‌​‌​​‌‌​​‌‌ Expedition 33
+-- AppID 1903340 | Generated on 2026-02-25 22:38 UTC | openlua.cloud
+
+-- Main Application
+addappid(1903340, 1, "302b1b9c8eb04ca24027876370a3072643790a12b835cabccaecb15b226075e8")
+
+-- Content Depots (1)
+addappid(1903341, 1, "8593f9c4cd595f833d5344e55cd6c6fdb73d9a4fa05dab033ba71622265e8c82") -- Clair Obscur: Expedition 33 - Content
+
+-- DLCs without Dedicated Depots (1)
+addappid(3228520) -- Clair Obscur Expedition 33  Deluxe Edition Upgrade
