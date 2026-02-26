2a569e61fa366a833a387a328341d19bd1401a04
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-25 19:51:33 -0500

    Add files via upload

---
+++ tokens/Reanimal-2129530.lua
@@ -0,0 +1,12 @@
+-- REA​‌‌​​​‌‌​​‌‌​​​​​‌‌​​‌​​​​‌‌‌​​‌​‌‌​​‌​​​‌‌​​​​‌​‌‌​​‌‌​​​‌‌​​‌‌​‌‌​​‌‌​​‌‌​​‌‌​​‌‌​​​​‌​​‌‌‌​​‌​‌‌​​‌​‌​​‌‌​‌‌‌​‌‌​​​‌​​​‌‌​​​‌NIMAL
+-- AppID 2129530 | Generated on 2026-02-25 15:42 UTC | openlua.cloud
+
+-- Main Application
+addappid(2129530, 1, "ea029714a30e4f9e526b5aa68792282d1ab1e7922fbba70da3983c9f958dcacb")
+
+-- Content Depots (1)
+addappid(2129531, 1, "8c619e3fd74002747f22974390275782dc2b568bc92b52859f0c5688a4a663f7") -- REANIMAL - Windows
+
+-- DLCs without Dedicated Depots (2)
+addappid(4112550) -- REANIMAL  Season Pass
+addappid(3954470) -- REANIMAL - Foxhead and Muttonhead Masks
