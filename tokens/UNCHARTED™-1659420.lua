b8be773bc3d1e812e6c7915fc08f82c7289e5ca4
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-27 03:23:48 -0500

    Add files via upload

---
+++ tokens/UNCHARTED™-1659420.lua
@@ -0,0 +1,10 @@
+-- UNCHARTED™: Legacy ​​‌‌​​​​​​‌‌​​​‌​​‌‌​​‌‌​‌‌​​‌‌​​​‌‌​​​​​‌‌​​​​‌​​‌‌​​‌‌​​‌‌​‌‌​​‌‌​​​‌‌​‌‌​​​‌‌​‌‌​​‌​​​​‌‌​‌‌​​‌‌​​‌​​​‌‌​​​‌​​​‌‌​‌​​​​‌‌​‌​‌of Thieves Collection
+-- AppID 1659420 | Generated on 2026-02-27 08:22 UTC | openlua.cloud
+
+-- Main Application
+addappid(1659420)
+
+-- Content Depots (3)
+addappid(1659421, 1, "54f1e37db693643220de1240ebc50a0c00d4bc3a53ffdd91cf3f52cc71c91382") -- UNCHARTED™: Legacy of Thieves Collection - Windows
+addappid(1659422, 1, "513eb93ddc7de41caabd6dfc7f8b2f5da716cc75ca9df1df674e4e339fceada2") -- UNCHARTED™: Legacy of Thieves Collection - Windows
+addappid(1659425, 1, "6bc8b76c2fb8d3e01a2bb8698af8b91196dbcb08fe353fcf64b55649ae766043") -- UNCHARTED™: Legacy of Thieves Collection - Windows
