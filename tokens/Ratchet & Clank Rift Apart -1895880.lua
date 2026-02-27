2eb22a2369a6411c54637ab59e3105978dbb7b83
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-26 19:21:13 -0500

    Add files via upload

---
+++ tokens/Ratchet & Clank Rift Apart -1895880.lua
@@ -0,0 +1,30 @@
+-- Ratchet & Cla​​‌‌​​​‌​​‌‌‌​​​​‌‌​​​​‌​​‌‌‌​​‌​‌‌​​‌​‌​‌‌​​​‌​​​‌‌​‌‌‌​‌‌​​​​‌​​‌‌‌​​‌​‌‌​​‌​​​​‌‌​​‌‌​​‌‌​​‌​​‌‌​​‌‌​​​‌‌​​‌‌​​‌‌​‌​‌​‌‌​​​‌​nk: Rift Apart
+-- AppID 1895880 | Generated on 2026-02-27 00:15 UTC | openlua.cloud
+-- Note: This game supports Steam Workshop content
+
+-- Main Application
+addappid(1895880, 1, "473df32c3da7cb2740c0c0ac161b4a136634d0f3cc738811b963f124ee1affda")
+
+-- Content Depots (19)
+addappid(1895881, 1, "47591f17983db571ad7f593c74995675394decc869f130ef55fba97e44b9061e") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895882, 1, "fa1c024080888563fe4764132e611d0df20a3981299e4cf4302a70d998f5f0e2") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895883, 1, "d5fc48ab7d1d44287e5810d1b12e2db1990512823e7d7b30eb1c3fa755a12a5a") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895884, 1, "1d79278b2659dcd81c71f9d9571a47e2c030a3a6bd9403dfd71a6adb0215327b") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895885, 1, "c437a13b614653ad14b067dd17621e76a6f5011fea0fb2e42c928bc3fe6a9a36") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895886, 1, "cffbe2e67d1bcb7575c172df0561c6b95d8b876edc74c803a9c7ae5a2c64ff10") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895887, 1, "14414d57bfec7c5cecd3d3bf5f826b99bc710507ca996474872d5533a7e7ba2a") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895888, 1, "95e486992749fc0285fdf61391f08501ee79bcafb6620e75274665c604d3ec60") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1895889, 1, "1830512e92e59ed89b07658cc940bddb5c14568c42cd0f9592b260b5e9aa8efa") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911671, 1, "acc30669dd53f17dafd22869c3097efcd33351f1142c1f3358069c2b0a5bcb09") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911672, 1, "ce45c0a29d3f657e7d8f1e951f7c4930544ae04f3f1e9224473185c50b23e6ca") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911673, 1, "cc63a107b4e2a473654e0e135eb8a18a78e176984fe7fe21352f4cf453e4628e") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911674, 1, "d8dd17fc2fc87b4f60142b2dd5f8e7779c856cc83108cdaffa6293f46f70f506") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911675, 1, "694b95f5929f709ce82dd2764b83788d14a673b6f1f5658f424e6ce04dae8b8a") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911676, 1, "5043823e008bfbc8ec48db007278c8f5db1a688c271244ad47b3adfc0a266f7e") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911677, 1, "7a6dbfe170eb680f61a4ef9f6ad91ddfffe71c421d862769debc696ba857e79e") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911678, 1, "1e0dfacf51dbd3aea352f0dc3b550ed334cd3b70cc5d6542806873f4ac8f0ce2") -- Ratchet & Clank: Rift Apart - Windows
+addappid(1911679, 1, "2de6a13a3246ad5a693ea54d60e165dfddf5e7e032df778a93e5dc25c4f67e1d") -- Ratchet & Clank: Rift Apart - Windows
+addappid(2202550, 1, "2c8181bb9254473772aa5abbd4cbafb2b7a830f967ee4e2739248431a76e1b87") -- Ratchet & Clank: Rift Apart - Windows
+
+-- DLCs without Dedicated Depots (1)
+addappid(2366780) -- Ratchet  Clank Rift Apart - Pre-Purchase Entitlements
