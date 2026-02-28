69149690d3680a0e7113b73a78c611ea971a54bb
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-28 18:33:32 -0500

    Add files via upload

---
+++ tokens/Dragon´s Dogma 2-2054970.lua
@@ -0,0 +1,74 @@
+-- Dragon'​​‌‌​​​‌​​‌‌​‌​‌​​‌‌‌​​​​​‌‌​‌​​​​‌‌‌​​​​‌‌​​‌​​​​‌‌​​‌‌​‌‌​​​‌‌​​‌‌​‌​‌​‌‌​​‌‌​​​‌‌‌​​​​​‌‌​​​​​‌‌​​‌‌​​​‌‌​‌​​​​‌‌​​‌​​​‌‌​​‌‌s Dogma 2
+-- AppID 2054970 | Generated on 2026-02-28 23:31 UTC | openlua.cloud
+-- Warning: This game uses DRM (Denuvo Anti-Tamper)
+
+-- Main Application
+addappid(2054970)
+
+-- Content Depots (4)
+addappid(2054971, 1, "792bf9022d9b26150098ba84f896617faee1db94dd4bd50cb9d70fcba3700282") -- Dragon's Dogma 2 - Content
+addappid(2054972, 1, "251b0eee00d4ff779243b1018a8c6c77664e933ba7ee3753803555cef509480f") -- Dragon's Dogma 2 - Content
+addappid(2054973, 1, "26687268229e99b88102170590ad39df0aa4bbc80190480af56ee3c54c11b1cc") -- Dragon's Dogma 2 - Content
+addappid(2054974, 1, "4ebc5021915c7e4cc3c5ba1a4a08015289788d290c3ed14a772893ee8ebb6a60") -- Dragon's Dogma 2 - Content
+
+-- DLCs with Content (23)
+addappid(2757120, 1, "53e2650ec1e211fcc14e5bab77bc4d54d48e31d6c8da6a97a9b8a91a4fa02a96") -- Dragons Dogma 2 1500 Rift Crystals - Points to Spend Beyond the Rift (B)
+addappid(2581000, 1, "34f71230c21f8dd552d994e4b39d8f177624b293956965b29c56fe5cad1afba1") -- Dragons Dogma 2 Superior Weapons Quartet - A Boon in Early Battles
+addappid(2757130, 1, "57a6b39b28c0ab8afb7ae9a1ea615edaee3a40530a9882be86b7ad0cadbfdec6") -- Dragons Dogma 2 1500 Rift Crystals - Points to Spend Beyond the Rift (C)
+addappid(2593170, 1, "6fb4131955e3f47efd1bc3d718bcba8b98a47551581d0ed68e4175cb02c0762d") -- Dragons Dogma 2 Ring of Assurance - Adventurers Safeguard
+addappid(2757140, 1, "63fd3570ac475c198512ff0e11cb8d6b6ea4b49746e34b4270de6dac67ac7f13") -- Dragons Dogma 2 1500 Rift Crystals - Points to Spend Beyond the Rift (D)
+addappid(2593180, 1, "94866b024a8c73d4c231e6399db5db65b8dba78f2b5a7324b953ba24e7580297") -- Dragons Dogma 2 Explorers Camping Kit - Camping Gear
+addappid(2694820, 1, "2ff10651137a55b9ea55d673f207d1a8f90d82b128b1eabec8cc296ce2a1ac64") -- Dragons Dogma 2 Wakestone - Restore the dead to life (B)
+addappid(2593190, 1, "717cfa28b5def3ef62181fa383c948ab68dc5ecf102dc498aa77e17c7c823d67") -- Dragons Dogma 2 Dragons Dogma Music  Sound Collection - Custom Sounds
+addappid(2694830, 1, "fa2ad1a149ada24a69bb6a644752e962e197779bef21e3d2b2f55b1746870a97") -- Dragons Dogma 2 Wakestone - Restore the dead to life (C)
+addappid(2593200, 1, "675ab608cb1b19781010a72514f455535f7951600ee60e6e900a7cd8cccf3958") -- Dragons Dogma 2 Harpysnare Smoke Beacons - Harpy Lure Item
+addappid(2757170, 1, "005ecbf1bfbad5b8d86810a628037653c0639076bbd85de52088656954a1f670") -- Dragons Dogma 2 2500 Rift Crystals - Points to Spend Beyond the Rift (A)
+addappid(2694840, 1, "8b651167e84540b7b94b5c38533869ee2d15c6705401e5b02f69738c8a9827f0") -- Dragons Dogma 2 Wakestone - Restore the dead to life (D)
+addappid(2593210, 1, "a94822751d8c9f35b041fe98b3766bbda91770fa3e689d7731b55aa770055906") -- Dragons Dogma 2 Heartfelt Pendant - A Thoughtful Gift
+addappid(2694850, 1, "8ef6554b2f36cd1f0edb13096eecc0f9aa4dbe9c979d5585bc28acf0f08a85c5") -- Dragons Dogma 2 Wakestone - Restore the dead to life (E)
+addappid(2593220, 1, "b4bd996b7a1391fbd2640f3f777dfa348338c316e2644a9106b88ecd2b1cdc18") -- Dragons Dogma 2 Ambivalent Rift Incense - Change Pawn Inclinations
+addappid(2757060, 1, "7eda6a67ceb23166e64a211e8b17e4e16cff894abd4064feb8b0be7ea53418d4") -- Dragons Dogma 2 500 Rift Crystals - Points to Spend Beyond the Rift (A)
+addappid(2593230, 1, "aa7db1a358553090e520148471ae0f47c415b4f234a3209fb9079ae5b47cc5a9") -- Dragons Dogma 2 Makeshift Gaol Key - Escape from gaol
+addappid(2757080, 1, "42737857648e91dadef02a18d35d2d4e33c0b869534e25508f47cb9b17ce22b5") -- Dragons Dogma 2 500 Rift Crystals - Points to Spend Beyond the Rift (B)
+addappid(2593240, 1, "0442eb19917ae6c4ba9b8cf177aab59bea31734ed109f700c910eac48a28e8b3") -- Dragons Dogma 2 Art of Metamorphosis - Character Editor
+addappid(2593250, 1, "ccc0a1a1c5ec9194116dc5b699892d4230f059f0b0ac053e54927446b214fcee") -- Dragons Dogma 2 1500 Rift Crystals - Points to Spend Beyond the Rift (A)
+addappid(2757090, 1, "d3c730e0273b41df400e908f2f19991dee84dacf09f756d250ee3bfcecfc86c1") -- Dragons Dogma 2 500 Rift Crystals - Points to Spend Beyond the Rift (C)
+addappid(2593260, 1, "0fe1b3dc096bdf30e4be2f6afd8f7e5073f1c5ec1b2a773435421a30af259437") -- Dragons Dogma 2 Wakestone - Restore the dead to life (A)
+addappid(2593270, 1, "ddc76b8dc500c37d77e1ef1a22be78b9470043c9e9d324a4cea0aa5b405e01e8") -- Dragons Dogma 2 Portcrystal - Warp Location Marker
+
+-- Version Lock
+setManifestid(2054971, 3136688068330793105)
+setManifestid(2054972, 1800823946498701498)
+setManifestid(2054973, 4686089925315080392)
+setManifestid(2054974, 119972914902696764)
+setManifestid(2581000, 3326500289311024291)
+setManifestid(2593170, 6723973524008089229)
+setManifestid(2593180, 2634842059355884280)
+setManifestid(2593190, 7277756736244661036)
+setManifestid(2593200, 3869936518091417960)
+setManifestid(2593210, 5414532205501697728)
+setManifestid(2593220, 7455982328362794134)
+setManifestid(2593230, 7251928774923524569)
+setManifestid(2593240, 315069544162243504)
+setManifestid(2593250, 967135362921279442)
+setManifestid(2593260, 69183187290100698)
+setManifestid(2593270, 2207132955792828001)
+setManifestid(2694820, 5885578699773604089)
+setManifestid(2694830, 1601933322779527921)
+setManifestid(2694840, 211900286292596559)
+setManifestid(2694850, 8403226506099305605)
+setManifestid(2757060, 760063107766921264)
+setManifestid(2757080, 5169243754881054003)
+setManifestid(2757090, 3444076000364683673)
+setManifestid(2757100, 3589438684669064439)
+setManifestid(2757110, 6993325223339298221)
+setManifestid(2757120, 1463489043546813420)
+setManifestid(2757130, 771876390790049210)
+setManifestid(2757140, 1033236542373376685)
+setManifestid(2757150, 4434377146123567945)
+setManifestid(2757160, 3279848405850203304)
+setManifestid(2757170, 3459631589371686775)
+setManifestid(2757180, 5271075382477428458)
+setManifestid(2757190, 7353387220959454102)
+setManifestid(2757200, 7794411242485950378)
+setManifestid(2757210, 6663042780467633977)
