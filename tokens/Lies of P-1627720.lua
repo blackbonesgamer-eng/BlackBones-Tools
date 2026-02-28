341cb98b390c3abe01dee9a053c462b3ef2f1b4d
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-02-28 17:13:24 -0500

    Add files via upload

---
+++ tokens/Lies of P-1627720.lua
@@ -0,0 +1,15 @@
+-- Lies​​‌‌​​​‌​​‌‌​​​​​‌‌​​​‌​​​‌‌​‌​‌​‌‌​​‌​‌​​‌‌​‌​​​​‌‌​​​‌​​‌‌​​‌‌​​‌‌‌​​‌​​‌‌​​​‌​​‌‌‌​​‌​​‌‌​‌​‌​‌‌​​‌​​​​‌‌​​​‌​​‌‌‌​​​​​‌‌​​​‌ of P
+-- AppID 1627720 | Generated on 2026-02-28 22:11 UTC | openlua.cloud
+
+-- Main Application
+addappid(1627720)
+addtoken(1627720, "16027423782916783116")
+
+-- Content Depots (2)
+addappid(1627721, 1, "d847ff3494ad5b72d69b7b38c52474fdb22d437421b569013dd1508d8879f7c1") -- Lies of P - Content
+addappid(1627722, 1, "b2550c254da0b229ff5f5bb765b809d2fac5bd74a98ff3611b8cb8870e02aa6a") -- Lies of P - macOS
+
+-- DLCs without Dedicated Depots (3)
+addappid(2325280) -- Lies of P  Pre-Order Bonus
+addappid(2325281) -- Lies of P Prince of Krat Cosmetics Pack
+addappid(2848330) -- Lies of P Overture
---
+++ tokens/Resident Evil-Requiem-3764200.lua
@@ -0,0 +1,17 @@
+-- Resident E​‌‌​​​​‌​​‌‌​‌‌‌​​‌‌​‌​‌​‌‌​​​‌​​‌‌​​​​‌​‌‌​​​‌​​​‌‌‌​​​​‌‌​​‌​‌​​‌‌​​‌‌​​‌‌​‌​​​​‌‌​​​​​​‌‌​‌​​​​‌‌​‌‌‌​​‌‌‌​​‌​​‌‌​‌‌​​‌‌​​​​‌vil Requiem
+-- AppID 3764200 | Generated on 2026-02-28 10:00 UTC | openlua.cloud
+-- Warning: This game uses DRM (Denuvo Anti-Tamper)
+
+-- Main Application
+addappid(3764200)
+
+-- Content Depots (1)
+addappid(3764201, 1, "bdf443ffde6449192b9863c0fa5e3cda31fdaa5e4d33e5bcded62dc2085b7cb8") -- Resident Evil Requiem - Content
+
+-- Version Lock
+setManifestid(3764201, 5691830764668778509)
+setManifestid(3764202, 7450784517022361359)
+setManifestid(3764205, 4436866528626191669)
+setManifestid(3764206, 136565968785594279)
+setManifestid(3990800, 9181787779883827112)
+setManifestid(3990820, 509708311968316285)
