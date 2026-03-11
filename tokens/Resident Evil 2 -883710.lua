e92d650c49423c0f2c9681da330b1338453ac3b0
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-03-11 14:52:09 -0500

    Add files via upload

---
+++ tokens/Resident Evil 2 -883710.lua
@@ -0,0 +1,34 @@
+-- Residen​​‌‌​‌‌​​‌‌​​‌‌​​​‌‌​‌​‌​​‌‌​‌​​​​‌‌​‌‌‌​‌‌​​​‌​​​‌‌​‌‌‌​​‌‌​​​‌​​‌‌​​‌​​​‌‌​‌‌​​​‌‌​​​​​​‌‌​​‌‌​‌‌​​​‌‌​​‌‌‌​​​​‌‌​​‌‌​​​‌‌​​​​t Evil 2
+-- AppID 883710 | Generated on 2026-03-10 22:03 UTC | openlua.cloud
+
+-- Main Application
+addappid(883710)
+
+-- Content Depots (11)
+addappid(883711, 1, "638f06d7569ecead867a27e5934284446cbf9a8740638265114ad27fb4ce2f27") -- RESIDENT EVIL 2
+addappid(883712, 1, "64aebcf6ac355fbc07d5edeb037ef5d1ecdd3415cc520f397e3674edd078fd84") -- BIOHAZARD RE:2
+addappid(883713, 1, "f0afbce3fe63581d5b1179fe571e137761ed6ca98455ee6606650f4b09f5249e") -- RESIDENT EVIL 2 TEST1
+addappid(883714, 1, "27631926b9162a5355df9eb09fdd90efe7631dcb86a2a34edbbd04fdb3ddbe02") -- BIOHAZARD RE:2 TEST1
+addappid(883715, 1, "a8afa9147111fd2590f12a2efafb74a91f4c9ac15eb56cdaaee4eb70b8b513ee") -- RESIDENT EVIL 2 TEST2
+addappid(883716, 1, "cdbca1dcd27611eab5a12f53db738d8d0012572146b930e71578f21d7662317c") -- BIOHAZARD RE:2 TEST2
+addappid(883717, 1, "66abdff706240ae3c2faa39ccb3d9cf90c2ba61cf80d0b94b5e40fc467e8dab5") -- RESIDENT EVIL 2 TEST3
+addappid(883718, 1, "bed9e589ec8c4a42f4f4c4165ea55ef524cc1a746466f0cd0dcec717d7422fd0") -- BIOHAZARD RE:2 TEST3
+addappid(883719, 1, "efe4f1a93047a0d60991536337e376ccc14e11b701a2a7b9140b0080c53bf5ea") -- SERIAL
+addappid(920572, 1, "dbd486a37d92e45a6cb1125e71b80bd31ce74e3732a72384b26103d71f27af5c") -- RESIDENT EVIL 2 WallpaperPack
+addappid(920573, 1, "a440dc284213e39f98528e058fa3457f1c6a9bbfbc97f6794a6b81f46056df2b") -- BIOHAZARD RE:2 WallpaperPack
+
+-- DLCs with Content (12)
+addappid(920560, 1, "ed972f698b50ed84c8edc351a84adc57ef81a9d75dfdc05f87297e2caa4741e3") -- Resident Evil 2 - Leon Costume Arklay Sheriff
+addappid(920561, 1, "bb62b32357a66bdfb10eb2a374912ffa9ba58924252603e1935226520aea8513") -- Resident Evil 2 - Leon Costume Noir
+addappid(920562, 1, "71020da471eab8cf287e8b1364dc41774641785316eef961b1bf872b2e81426b") -- Resident Evil 2 - Claire Costume Military
+addappid(920563, 1, "e970b08de60e4726cf9020d6eba9cc48c1dc79053e7000033ddb28c328c7ee34") -- Resident Evil 2 - Claire Costume Noir
+addappid(920564, 1, "194df6fccffb69f0b91badf8550570c554f9dd2ba7a8ce025c23b2e1b707de51") -- Resident Evil 2 - Claire Costume Elza Walker
+addappid(920565, 1, "cc4cc3692f32ee8a0e912af30b2a6c7d63ca6625dfe904bc9c8a7fe7e27c3f45") -- Resident Evil 2 - Leon Costume 98
+addappid(920566, 1, "747b360f6029581b9e552dbadd556684c85b21f80c6d812a6ea68f48b8e38630") -- Resident Evil 2 - Claire Costume 98
+addappid(920567, 1, "1cb02d129e55942d541b1f1c88365d043c8a0cce730a25ec3c56a15c77523def") -- Resident Evil 2 - Original Ver. Soundtrack Swap
+addappid(920568, 1, "dd350bfb23bfd3c931f2589bab2b0ce0eff6704491a777ff4db51439cb2bf94c") -- Resident Evil 2 - Deluxe Weapon Samurai Edge - Albert Model
+addtoken(920569, "9210640672866114780")
+addappid(920569, 1, "d5951968c5c68af4a7e4cca71625a71dfb289fc98e170a6e25e87221faac71f5") -- Resident Evil 2 - Deluxe Weapon Samurai Edge - Jill Model
+addtoken(920570, "12658968023885460684")
+addappid(920570, 1, "7b918b3037406996a31fddfade6f149ee7a41e6dff77548200886097deb98ec6") -- Resident Evil 2 - Deluxe Weapon Samurai Edge - Chris Model
+addappid(920571, 1, "04ba2f12b585affc9ed2cc03b5b04f136e6159ea7430892ae71c5a541c7ae427") -- Resident Evil 2 - All In-game Rewards Unlocked
