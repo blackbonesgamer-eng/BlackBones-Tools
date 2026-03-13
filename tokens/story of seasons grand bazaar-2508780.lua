a3f26584bd3826dceb03150cb088af36388c32c7
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-03-13 18:22:34 -0500

    Add files via upload

---
+++ tokens/story of seasons grand bazaar-2508780.lua
@@ -0,0 +1,17 @@
+-- STORY OF SEASO​​‌‌​​​​​​‌‌‌​​​​​‌‌​​‌‌​​‌‌​‌‌​​​‌‌‌​​​​‌‌​​​‌​​​‌‌​​‌​​​‌‌​​​‌​‌‌​​​‌​​‌‌​​‌​‌​​‌‌​‌‌​​‌‌​​‌​‌​​‌‌​‌‌​​‌‌​​‌‌​​​‌‌​​​​​​‌‌​‌​​NS: Grand Bazaar
+-- AppID 2508780 | Generated on 2026-03-13 23:21 UTC | openlua.cloud
+
+-- Main Application
+addappid(2508780)
+
+-- Content Depots (1)
+addappid(2508781, 1, "d3d2cecbf425b32af43928d0b54fb991edc1a74dfac4d72d31a47b22d078ec14") -- STORY OF SEASONS: Grand Bazaar - Windows
+
+-- DLCs with Content (4)
+addappid(3248040, 1, "d671f9d0718c441e13b212891d9003f5260e69433455f44bcf2c768e61173336") -- STORY OF SEASONS Grand Bazaar - Trunk of Transformation
+addappid(3482230, 1, "0789e54527d096025f42b5c9e7196eed567c1e7b5a83a8466121fe86a3b6a8bb") -- STORY OF SEASONS Grand Bazaar - Pine Hoverfly Set
+addappid(3847830, 1, "418b5c8fd01c80869d13b37d6ddff62844ed8fa89b0c9c3803a9846c36a96c3f") -- STORY OF SEASONS Grand Bazaar - Art Book
+addappid(3051830, 1, "11061a1ed5036b24ad17351d518ca68411d4fe283cf6156176f5d161b9044ebc") -- STORY OF SEASONS Grand Bazaar - Cow Set
+
+-- DLCs without Dedicated Depots (1)
+addappid(3847810) -- STORY OF SEASONS Grand Bazaar - Super Digital Deluxe DLC Set
