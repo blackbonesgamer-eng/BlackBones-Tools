84472e56cf89df57f90d081a3d65e6eca0795321
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-03-15 15:58:50 -0500

    Add files via upload

---
+++ tokens/MiSide-2527500.lua
@@ -0,0 +1,8 @@
+-- Mi​​‌‌​‌​‌​‌‌​​​‌​​​‌‌‌​​​​​‌‌​​‌‌​​‌‌‌​​‌​‌‌​​‌​​​​‌‌​‌​‌​‌‌​​‌​‌​​‌‌​​​‌​​‌‌​‌‌‌​​‌‌​‌‌‌​​‌‌​‌​‌​​‌‌​​‌‌​‌‌​​‌​​​​‌‌​​​‌​​‌‌‌​​‌Side
+-- AppID 2527500 | Generated on 2026-03-15 20:52 UTC | openlua.cloud
+
+-- Main Application
+addappid(2527500)
+
+-- Content Depots (1)
+addappid(2527501, 1, "ffef2d1f42cd2438f5b7d029d771a9f941fd224250c3184f595773fb4926e1cd") -- MiSide - Content
---
+++ tokens/Split Fiction-2001120.lua
@@ -0,0 +1,12 @@
+-- Split ​​‌‌​​​​​‌‌​​‌​‌​‌‌​​‌​​​​‌‌​‌​​​​‌‌‌​​​​‌‌​​‌​‌​​‌‌​​‌‌​​‌‌​‌‌​​​‌‌​‌‌​​​‌‌​​‌​​‌‌​​​​‌​‌‌​​​‌​​​‌‌​‌‌‌​​‌‌‌​​​​‌‌​​‌​​​​‌‌​​​​Fiction
+-- AppID 2001120 | Generated on 2026-03-15 20:50 UTC | openlua.cloud
+-- Warning: This game uses DRM (EA App)
+
+-- Main Application
+addappid(2001120)
+
+-- Content Depots (1)
+addappid(2001121, 1, "27f6aac2ce9d80ad4ed8fe3710d306ae108f00ddcf005d47ecf7bfa2a258964c") -- Split Fiction - Content
+
+-- DLCs without Dedicated Depots (1)
+addappid(3050960) -- Split Fiction - Full Game Unlock
