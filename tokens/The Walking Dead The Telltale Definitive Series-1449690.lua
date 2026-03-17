7d5beca720b27e5368879e9a61d6592ee177841f
blackbonesgamer-eng <blackbonesgamer@gmail.com>
2026-03-17 01:49:57 -0500

    Add files via upload

---
+++ tokens/The Walking Dead The Telltale Definitive Series-1449690.lua
@@ -0,0 +1,8 @@
+-- The Walking Dead: The T​​‌‌​​​‌​​‌‌​‌​‌​​‌‌​‌‌​​​‌‌​​​‌​​‌‌​‌​​​​‌‌‌​​​​​‌‌​‌​‌​​‌‌‌​​‌​​‌‌​​‌‌​​‌‌​‌​​​​‌‌​‌‌​​‌‌​​‌​‌​‌‌​​​‌​​​‌‌‌​​​​​‌‌​‌‌​​​‌‌​‌‌‌elltale Definitive Series
+-- AppID 1449690 | Generated on 2026-03-17 06:47 UTC | openlua.cloud
+
+-- Main Application
+addappid(1449690)
+
+-- Content Depots (1)
+addappid(1449691, 1, "ebfb9f62016b22f81e1323ccfc885d47a554513a3aec0310ea08e4043e4a84a7") -- The Walking Dead: The Telltale Definitive Series Content
